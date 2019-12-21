196a97bfebc6f64ff8743696e5aa7ff8970f9035
haizhou <haizhouren@gmail.com>
2019-12-21 19:18:05 +0800

    Origin

---
+++ enums.d.ts
@@ -0,0 +1,38 @@
+// Auto-generated. Do not edit.
+
+
+    declare const enum LCD_COLOR {
+    WHITE = 0x0000,
+    BLACK = 0xFFFF,
+    YELLOW = 0x001F,
+    CYAN = 0xF800,
+    GREEN = 0xF81F,
+    MAGENTA = 0x07E0,
+    RED = 0x7FFF,
+    BLUE = 0xFFE0,
+    GRAY = 0X8F08,
+    }
+
+
+    declare const enum DOT_PIXEL {
+    DOT_PIXEL_1 = 1,
+    DOT_PIXEL_2 = 2,
+    DOT_PIXEL_3 = 3,
+    DOT_PIXEL_4 = 4,
+    }
+
+
+    declare const enum LINE_STYLE {
+    LINE_SOLID = 0,
+    LINE_DOTTED = 1,
+    }
+
+
+    declare const enum DRAW_FILL {
+    DRAW_EMPTY = 0,
+    DRAW_FULL = 1,
+    }
+declare namespace LCD1IN8 {
+}
+
+// Auto-generated. Do not edit. Really.
